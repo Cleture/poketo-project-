51ca5f1f3b441b5f762713175b6efbcca7a75efa
elyonjohn <elyonjohn60@gmail.com>
2024-04-10 23:34:13 +0100

    Slider animation, Increment and Decrement QUantity In this commit i created a JS function foe the slider in the homepage, And also created an increment and decrement function for the addition of items in the product page

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('.logos-container').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+
+    function incrementQuantity() {
+        var input = document.querySelector('.quantity-input');
+        var value = parseInt(input.value);
+        input.value = value + 1;
+    }
+
+    function decrementQuantity() {
+        var input = document.querySelector('.quantity-input');
+        var value = parseInt(input.value);
+        if (value > 1) {
+            input.value = value - 1;
+        }
+    }
+});

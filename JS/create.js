75fc9d18d141536336cf8f478aef45eb54c9c96f
elyonjohn <elyonjohn60@gmail.com>
2024-04-30 21:44:02 +0100

    Working on product creation.

--- JS/create.js
+++ JS/create.js
@@ -1,25 +1,30 @@
 $(document).ready(function () {
     $("#blogBtn").click(function (e) {
         e.preventDefault();
-        // Check if user is logged in
-        let loggedUser = localStorage.getItem("OurMerchant_user");
+        // let loggedUser = JSON.parse(localStorage.getItem("OurMerchant_user"));
         if (loggedUser) {
-            let merchant_details = JSON.parse(loggedUser);
-            // Get product details
+            let merchant_details = loggedUser.id 
+            console.log(loggedUser.id);
             let productDetails = {
-                merchant_id: merchant_details.id,
+                merchant_id: merchant_details,
                 title: $("#title").val(),
-                description: $("#blog_body").val(),
+                description: $("#descp").val(),
+                price: $("#price").val(),
+                brand: $("#brand").val(),
+                quantity: $("#quantity").val(),
+                image: $("#image").val(),
+                currency: $("#currency").val(),
+                min_qty: $("#min-qnty").val(), // Corrected the input id
+                max_qty: $("#max-qnty").val(),
+                discount: $("#discount").val(),
+                discount_expiration: $("#discount-expire").val(),
                 category_id: $("#category").val(),
-                likes: 0,
-                dislikes: 0,
-                date_created: new Date().toLocaleDateString(),
             };
-            // Send AJAX request to create product
             $.ajax({
                 url: "http://ecommerce.reworkstaging.name.ng/v2/products",
                 method: "POST",
-                data: JSON.stringify(productDetails),
+                data: productDetails,
+                dataType: "json",
                 success: function () {
                     alert("Product created successfully");
                     window.location.href = "./dashboard.html";
@@ -33,7 +38,27 @@ $(document).ready(function () {
             alert("Please Login to create a product");
         }
     });
+
+    // Get All Categories
+    function getCategories() {
+        $.ajax({
+            url: "http://ecommerce.reworkstaging.name.ng/v2/categories?merchant_id=662fcfc1f7192e0c5d709b4b",
+            method: "GET",
+            success: function (data) {
+                let output1 = "";
+                let output2 = "";
+                $(data).each(function (index, element) {
+                    output1 += `<li><a href="">${element.name}</a></li>`;
+                    output2 += `<option value="${element.id}">${element.name}</option>`;
+                });
+                $("#category").html(output2); // Append the options to the select element
+            },
+        });
+    }
+    getCategories();
 });
+
+
 
 
 
